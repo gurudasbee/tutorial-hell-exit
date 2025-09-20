ddaaa404e93efa6aec613c25c475f7772b2143da
hive-cmky <devabhijit597@gmail.com>
2025-09-20 22:47:19 +0530

    Some more files added to my first react project.

--- website/my-react-app3/src/App.jsx
+++ website/my-react-app3/src/App.jsx
@@ -1,9 +1,18 @@
 // import Button from "./Button/Button.jsx"
-import Button from "./Button";
+// import Button from "./Button"
+import Student from "./Student.jsx"
+
 
 function App() {
   return (
-  <Button />
+  <>
+    {/* <Button /> */}
+    <Student name="Spongebob" age={34} isStudent={true} />
+    <Student name="Patrick" age={42} isStudent={false} />
+    <Student name="Squidware" age={50} isStudent={false} />
+    <Student name="Sandy" age={27} isStudent={true} />
+    <Student />
+  </>
   );
  }
 
